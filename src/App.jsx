c40555a3a140b0a2548082fdf7b6211117ecfd42
gohjinzhu <gohjinzhu@hotmail.com>
2023-01-12 14:52:36 +0800

    init, main structure

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import "@fontsource/righteous"
+import "@fontsource/roboto"
+
+function App() {
+  const [input, setInput] = useState(0)
+  
+
+  return (
+    <div className="App">
+      <div className='heading'>WORD MASTER</div>
+      <div className='body'>
+        <div className='wrapper'>
+          <span className='grid-item'>S</span>
+          <span className='grid-item'>S</span>
+          <span className='grid-item'>S</span>
+          <span className='grid-item'>S</span>
+          <span className='grid-item'>S</span>
+          <span className='grid-item'>S</span>
+          <span className='grid-item'>S</span>
+          <span className='grid-item'>S</span>
+          <span className='grid-item'>S</span>
+          <span className='grid-item'>S</span>
+          <span className='grid-item'>S</span>
+          <span className='grid-item'>S</span>
+          <span className='grid-item'>S</span>
+          <span className='grid-item'>S</span>
+          <span className='grid-item'>S</span>
+          <span className='grid-item'>S</span>
+          <span className='grid-item'>S</span>
+          <span className='grid-item'>S</span>
+          <span className='grid-item'>S</span>
+          <span className='grid-item'>S</span>
+          <span className='grid-item'>S</span>
+          <span className='grid-item'>S</span>
+          <span className='grid-item'>S</span>
+          <span className='grid-item'>S</span>
+          <span className='grid-item'>S</span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
